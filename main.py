f5ac532f3b9ace50b2e2fd2f332716b055c66098
deutho <59829810+deutho@users.noreply.github.com>
2022-01-12 14:14:51 +0100

    looong loop

--- main.py
+++ main.py
@@ -7,6 +7,6 @@ print("Hello World!")
 sensor = sensor_CCS811()
 print("I am done with the sensor")
 counter = 0
-while counter < 10:
+while counter < 10000:
     sensor.update_telemetry()
     counter = counter + 1
