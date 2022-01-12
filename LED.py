32beeb005a84e2800b1d5414d240e94de5b407e2
deutho <59829810+deutho@users.noreply.github.com>
2022-01-12 15:00:35 +0100

    test Fan

--- LED.py
+++ LED.py
@@ -1,10 +1,17 @@
-import RPi.GPIO as GPIO
+# import RPi.GPIO as GPIO
+from gpiozero import OutputDevice
 import time
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-GPIO.setup(18,GPIO.OUT)
-print("LED on")
-GPIO.output(18,GPIO.HIGH)
-time.sleep(3)
-print("LED off")
-GPIO.output(18,GPIO.LOW)
+# GPIO.setmode(GPIO.BCM)
+# GPIO.setwarnings(False)
+# GPIO.setup(18,GPIO.OUT)
+# print("LED on")
+# GPIO.output(18,GPIO.HIGH)
+# time.sleep(3)
+# print("LED off")
+# GPIO.output(18,GPIO.LOW)
+
+
+fan = OutputDevice(26)
+fan.on()
+time.sleep(10)
+fan.off()
