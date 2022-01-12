4e6679d4dcb19f31d2d9058a975c95faf902a4ca
deutho <59829810+deutho@users.noreply.github.com>
2022-01-12 15:46:48 +0100

    looping temperature 10 times

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from CCS811 import sensor_CCS811
-
+import time
 
 print("Hello World!")
 
@@ -7,3 +7,7 @@ print("Hello World!")
 sensor = sensor_CCS811()
 print("I am done with the sensor")
 #hallo
+
+for x in range(10):
+    sensor.update_telemetry()
+    time.sleep(1)
