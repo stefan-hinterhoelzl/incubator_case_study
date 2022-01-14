9ff424808efb5458e3d35763e6488ebad001a433
stefan-hinterhoelzl <59830330+stefan-hinterhoelzl@users.noreply.github.com>
2022-01-14 15:06:54 +0100

    publisher

--- physical_twin/publisher.py
+++ physical_twin/publisher.py
@@ -8,17 +8,22 @@ MQTT_SERVER = "localhost"
 MQTT_PATH = "data"
 sensor = sensor_CCS811()
 
-def on_connect(client, userdata, flags, rc):
-    print("Connected with result code "+str(rc))
 
-
+def publish(client):
     message = sensor.update_telemetry()
     print(message)
     client.publish(MQTT_PATH, payload = message, qos = 0, retain=False)
 
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+
+    publish(client)
 
 def on_publish(client, userdata, msg):
     print("message_sent")
+    time.sleep(3)
+    publish(client)
+
 
 client = mqtt.Client("client1")
 client.on_connect = on_connect
