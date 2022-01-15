a0d97271322cfe1b7075d7b546d1437747764841
stefan-hinterhoelzl <59830330+stefan-hinterhoelzl@users.noreply.github.com>
2022-01-15 22:18:55 +0100

    quick check

--- physical_twin/LowLevelDriver.py
+++ physical_twin/LowLevelDriver.py
@@ -11,37 +11,37 @@ class LowLevelDriver:
         global fanState
         fanState = "OFF"
 
-    def heaterON():
+    def heaterON(self):
         heater = OutputDevice(27)
         global heaterState
         if (heaterState == "OFF"):
             heater.on()
             heaterState = "ON"
 
-    def heaterOFF():
+    def heaterOFF(self):
         heater = OutputDevice(27)
         global heaterState
         if (heaterState == "ON"):
             heater.off()
             heaterState = "OFF"
 
-    def fanON():
+    def fanON(self):
         fan = OutputDevice(17)
         global fanState
         if (fanState == "OFF"):
             fan.on()
             fanState = "ON"
 
-    def fanOFF():
+    def fanOFF(self):
         fan = OutputDevice(17)
         global fanState
         if (fanState == "ON"):
             fan.off()
             fanState = "OFF"
 
-    def getHeaterState():
+    def getHeaterState(self):
         return heaterState
 
-    def getFanState():
+    def getFanState(self):
         return fanState
 
