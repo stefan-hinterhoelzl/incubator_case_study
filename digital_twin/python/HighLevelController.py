6b7e882a836eed625b36f1d3fb45c3bdb13a6edd
stefan-hinterhoelzl <59830330+stefan-hinterhoelzl@users.noreply.github.com>
2022-01-16 23:29:09 +0100

    try catch

--- digital_twin/python/HighLevelController.py
+++ digital_twin/python/HighLevelController.py
@@ -6,67 +6,72 @@ import json
 MQTT_SERVER = "192.168.11.15"
 MQTT_PATH_SUBSCRIBE = "data"
 MQTT_PATH_PUBLISH = "commands"
-LOWERBOUND = 26
+LOWERBOUND = 27
 HIGHERBOUND = 28
 CURRENTSTATE = ""
 
 
-def on_connect(client, userdata, flags, rc):
-    print("Connected with result code "+str(rc))
+try:
 
-    client.subscribe(MQTT_PATH_SUBSCRIBE)
+    def on_connect(client, userdata, flags, rc):
+        print("Connected with result code "+str(rc))
 
-def on_message(client, userdata, msg):
+        client.subscribe(MQTT_PATH_SUBSCRIBE)
 
-    m_in = json.loads(msg.payload.decode())
-    print(m_in)
+    def on_message(client, userdata, msg):
 
-    data = {}
+        m_in = json.loads(msg.payload.decode())
+        print(m_in)
 
-    global CURRENTSTATE
-    if (CURRENTSTATE == ""):
-        if (float(m_in["temp"]) < HIGHERBOUND):
-            CURRENTSTATE = "H"
+        data = {}
+
+        global CURRENTSTATE
+        if (CURRENTSTATE == ""):
+            if (float(m_in["temp"]) < HIGHERBOUND):
+                CURRENTSTATE = "H"
+            else:
+                CURRENTSTATE = "F"
+
+
+        if (CURRENTSTATE == "H"):
+            if (float(m_in["temp"]) < HIGHERBOUND):
+                data["H"] = 1
+                data["F"] = 0
+            else:
+                data["H"] = 0
+                data["F"] = 1
+                CURRENTSTATE = "F"
+
         else:
-            CURRENTSTATE = "F"
-
-
-    if (CURRENTSTATE == "H"):
-        if (float(m_in["temp"]) < HIGHERBOUND):
-            data["H"] = 1
-            data["F"] = 0
-        else:
-            data["H"] = 0
-            data["F"] = 1
-            CURRENTSTATE = "F"
-
-    else:
-        if (float(m_in["temp"]) < LOWERBOUND):
-            data["H"] = 1
-            data["F"] = 0
-            CURRENTSTATE = "H"
-        else:
-            data["H"] = 0
-            data["F"] = 1
+            if (float(m_in["temp"]) < LOWERBOUND):
+                data["H"] = 1
+                data["F"] = 0
+                CURRENTSTATE = "H"
+            else:
+                data["H"] = 0
+                data["F"] = 1
+                
             
+
         
-
-    
-    json_object = json.dumps(data)
-    
-    client.publish(MQTT_PATH_PUBLISH, payload = json_object, qos = 0, retain=False)
+        json_object = json.dumps(data)
+        
+        client.publish(MQTT_PATH_PUBLISH, payload = json_object, qos = 0, retain=False)
 
 
-def on_publish(client, userdata, result):
-   pass
+    def on_publish(client, userdata, result):
+        pass
 
-client = mqtt.Client()
-client.on_connect = on_connect
-client.on_message = on_message
-client.on_publish = on_publish
+    client = mqtt.Client()
+    client.on_connect = on_connect
+    client.on_message = on_message
+    client.on_publish = on_publish
 
-client.username_pw_set(username = "admin", password = "admin")
+    client.username_pw_set(username = "admin", password = "admin")
 
-client.connect(MQTT_SERVER, 1883, 60)
+    client.connect(MQTT_SERVER, 1883, 60)
 
-client.loop_forever()
+    client.loop_forever()
+
+except KeyboardInterrupt:
+    print("Programm was manually stopped")
