58090e2f541b9628edb33de4a9f22b07057d06be
stefan-hinterhoelzl <59830330+stefan-hinterhoelzl@users.noreply.github.com>
2022-01-16 10:34:04 +0100

    testing

--- physical_twin/turnOff.py
+++ physical_twin/turnOff.py
@@ -6,3 +6,12 @@ GPIO.setup(27, GPIO.OUT)
 
 GPIO.output(17, GPIO.LOW)
 GPIO.output(27, GPIO.LOW)
+
+print(GPIO.input(17))
+print(GPIO.input(27))
+
+GPIO.output(17, GPIO.HIGH)
+GPIO.output(27, GPIO.HIGH)
+
+print(GPIO.input(17))
+print(GPIO.input(27))
